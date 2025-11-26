afe95856cd5aedf5da0645e653905d35deb8c30f
Akhil Rai <raiakhil2627@gmail.com>
2025-11-26 11:40:02 +0530

    Assignment completed

--- Assignment/CustomMyMap.js
+++
@@ -1,9 +0,0 @@
-// Q8 myMap implementation
-Array.prototype.myMap = function(callback) {
-  const output = [];
-  for(let i=0;i<this.length;i++){
-    output.push(callback(this[i], i, this));
-  }
-  return output;
-};
-console.log([1,2,3].myMap(n=>n*2));
--- Assignment/MakeMultiplier.js
+++
@@ -1,8 +0,0 @@
-// Q7 makeMultiplier closure
-function makeMultiplier(multiplier) {
-  return function(num) {
-    return num * multiplier; // closure captures multiplier
-  };
-}
-const triple = makeMultiplier(3);
-console.log(triple(5));
